6ee272eaa2cc8be96210cee66b7c0c25ead073fb
Jeevan KC <jkc5186@gmail.com>
2025-01-30 21:17:07 +0545

    here

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
   latex: true
-})
+});
 
-module.exports = withNextra()
+module.exports = withNextra({
+  output: 'standalone', // Optimized for Vercel
+  revalidate: 10, // Enable ISR (regenerates pages every 10 seconds)
+});
